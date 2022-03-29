ba14b787550f8e740f9d0665b2c3be5b6dd3c558
BT1E10_18 <sample@bths.edu>
2022-03-29 11:01:46 -0400

    Initial commit

---
+++ src/Cabaret.java
@@ -0,0 +1,32 @@
+import java.util.ArrayLists;
+import java.util.Arrays;
+
+public class Cabaret
+{
+    private String name;
+    private ArrayList<Performer> performers;
+
+    public Cabaret(String name)
+    {
+        performers=new ArrayList<Performers>();
+        this.name=name;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public ArrayList<Performer> getPerformers()
+    {
+        return performers;
+    }
+
+    public boolean addPerformer(Performer performer)
+    {
+        if(performers.indexOf(performers))
+        {
+            
+        }
+    }
+}
